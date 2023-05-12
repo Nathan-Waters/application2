81e165492f5b65fabff2c6d3e2a812d5a7509d7f
Nathan Waters <waters.nathan@student.greenriver.edu>
2023-05-12 19:19:50 -0400

    finished styling for jobs page and made summary page + routing

--- index.php
+++ index.php
@@ -24,6 +24,7 @@ $f3->route('GET /', function() {
     $view = new Template();
     echo $view->render('views/home.html');
 });
+
 $f3->route('GET /home', function() {
 
     // Display a view page
@@ -32,11 +33,27 @@ $f3->route('GET /home', function() {
 });
 
 $f3->route('GET|POST /info', function($f3) {
-    var_dump($_SESSION['REQUEST_METHOD']);
-    var_dump($_POST);
+    var_dump($_SESSION);
+    var_dump($_GET);
 
-    if(sizeof($_POST)!=0){
+    if($_SERVER['REQUEST_METHOD'] == "POST"){
+        var_dump($_POST);
+        var_dump($_GET);
 
+        $fName = $_POST['fName'];
+        $f3->set('SESSION.fName', $fName);
+
+        $lName = $_POST['lName'];
+        $f3->set('SESSION.lName', $lName);
+
+        $email = $_POST['email'];
+        $f3->set('SESSION.email', $email);
+
+        $state = $_POST['state'];
+        $f3->set('SESSION.state', $state);
+
+        $phone = $_POST['phone'];
+        $f3->set('SESSION.phone', $phone);
 
         $f3->reroute('exp');
     }
@@ -46,9 +63,21 @@ $f3->route('GET|POST /info', function($f3) {
 });
 
 $f3->route('GET|POST /exp', function($f3) {
+    var_dump($_SESSION);
+    var_dump($_POST);
     // Display a view page
-    if(sizeof($_POST)!=0){
+    if($_SERVER['REQUEST_METHOD'] == "POST"){
+        $bio = $_POST['bio'];
+        $f3->set('SESSION.bio', $bio);
 
+        $github = $_POST['github'];
+        $f3->set('SESSION.github', $github);
+
+        $exp = $_POST['exp'];
+        $f3->set('SESSION.exp', $exp);
+
+        $relocate = $_POST['relocate'];
+        $f3->set('SESSION.relocate', $relocate);
 
         $f3->reroute('jobs');
     }
@@ -56,11 +85,33 @@ $f3->route('GET|POST /exp', function($f3) {
     echo $view->render('views/exp.html');
 });
 
-$f3->route('GET /jobs', function() {
+$f3->route('GET|POST /jobs', function($f3) {
+//    var_dump($_SESSION);
+//    var_dump($_POST);
     // Display a view page
 
+    if($_SERVER['REQUEST_METHOD'] == "POST"){
+            var_dump($_POST);
+//        $softwareJobs = $_POST['softwareJobs'];
+
+
+        $softwareJobs = implode(", ",$_POST['softwareJobs']);
+        $f3->set('SESSION.softwareJobs', $softwareJobs);
+
+        $specificJobs = implode(", ",$_POST['specificJobs']);
+        $f3->set('SESSION.specificJobs', $specificJobs);
+
+        $f3->reroute('summary');
+    }
     $view = new Template();
     echo $view->render('views/jobs.html');
+});
+
+$f3->route('GET /summary', function() {
+    var_dump($_SESSION['fName']);
+    var_dump($_POST);
+    $view = new Template();
+    echo $view->render('views/summary.html');
 });
 
 // Run Fat-Free
